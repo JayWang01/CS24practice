1b1cb037e2eeb799f8e65ce0dae255adab4bd2dd
JayWang01 <wsc2001math@126.com>
2020-07-09 04:51:37 -0700

    alter 1

---
+++ Chapter 3/bag.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+double x; 
+
+int main() {
+  cout << "please input a real number in [0, 2*pi]" <<n;
+  cin >> x; 
+  double y = sin(x);
+  cout<<"the sine of" << x << "is" << y <<endl;
+  return 0;
+}
+
+
